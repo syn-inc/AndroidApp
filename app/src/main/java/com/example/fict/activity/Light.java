104266296d964b23403903bb46a3b6732eeecf33
Alex Pro <dedifferentiator@gmail.com>
2019-03-20 18:18:21 +0200

    Init commit

--- app/src/main/java/com/example/fict/activity/Light.java
+++ app/src/main/java/com/example/fict/activity/Light.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.example.fict.MainActivity;
 import com.example.fict.R;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.example.fict.ValueFormatter;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static com.example.fict.MainActivity.getLIGHT;
 
@@ -27,34 +35,45 @@ public class Light extends AppCompatActivity {
         textView1.setText(date.toString());
         textView.setText(getLIGHT());
         updateLightGraph();
-
-
     }
-
     public void updateLightGraph() {
 
         // plotting results
-        GraphView graph = findViewById(R.id.graph_light);
-        initGraph(graph);
-    }
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+        Description desc = chart.getDescription();
+        desc.setText("");
 
-    public void initGraph(GraphView graph) {
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
 
-        DataPoint[] points = new DataPoint[30];
-        for (int i = 0; i < points.length; i++) {
-            points[i] = new DataPoint(i, Math.sin(i));
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setAxisMaximum(12);
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return MainActivity.getMonths()[(int) value % MainActivity.getMonths().length];
+            }
+        });
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        List<Entry> entries = new ArrayList<Entry>();
+
+        for (int i = 0; i < 50; i++) {
+            entries.add(new Entry((float) i, (float) Math.pow(Math.sin(i), 2)*25));
         }
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
-        series.setColor(Color.argb(255, 0, 255, 0));
-        series.setTitle("Light");
-
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(7);
-
-        graph.getViewport().setScrollable(true);
-        graph.addSeries(series);
-        graph.getLegendRenderer().setVisible(true);
-        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        LineDataSet dataSet = new LineDataSet(entries,"Quantity of light");
+        dataSet.setColor(Color.rgb(0, 255, 0));
+        dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setLineWidth(2.5f);
+        dataSet.setCircleRadius(4f);
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
     }
 }
