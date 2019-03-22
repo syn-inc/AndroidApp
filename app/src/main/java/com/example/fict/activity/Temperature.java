8ab41bf17e96fd6cceca7565c52b31806d5b47c5
Alex Pro <dedifferentiator@gmail.com>
2019-03-22 21:12:34 +0200

    improve temperature's graph

--- app/src/main/java/com/example/fict/activity/Temperature.java
+++ app/src/main/java/com/example/fict/activity/Temperature.java
@@ -2,15 +2,21 @@ package com.example.fict.activity;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.example.fict.MainActivity;
+import com.example.fict.MyMarkerView;
 import com.example.fict.Parsing;
 import com.example.fict.R;
+import com.example.fict.Respones;
 import com.example.fict.ValueFormatter;
-import com.example.fict.MainActivity;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
@@ -19,22 +25,30 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.example.fict.Respones;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import static com.example.fict.MainActivity.getTEMPEARTURE;
 
 public class Temperature extends AppCompatActivity {
-    ArrayList dayValueHistory;
+    ArrayList<Float> dayValueHistory;
+    private LineChart chart;
+    public static float minGraphValue = 0;
+    public static float maxGraphValue = 0;
 
-    public void setDayValueHistory(ArrayList dayValueHistory) {
+    public void setDayValueHistory(ArrayList<Float> dayValueHistory) {
         this.dayValueHistory = dayValueHistory;
     }
 
-    public ArrayList getDayValueHistory() {
+    public ArrayList<Float> getDayValueHistory() {
         return dayValueHistory;
     }
 
@@ -44,7 +58,7 @@ public class Temperature extends AppCompatActivity {
         setContentView(R.layout.activity_temperature);
 
         Date date = new Date();
-        TextView textView = findViewById(R.id.textView5);        //Find textview for display last value
+        TextView textView = findViewById(R.id.textView5);        //Find textView for display last value
         TextView textView1 = findViewById(R.id.Date);        //show date
         textView1.setText(date.toString());
         textView.setText(getTEMPEARTURE());         //set last value on the main screen
@@ -92,7 +106,12 @@ public class Temperature extends AppCompatActivity {
      * Creates temperature graph
      */
     public void createTempGraph() {
-        LineChart chart = findViewById(R.id.chart);
+
+        Calendar c = Calendar.getInstance();
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat("HH");
+        final int datetime = Integer.parseInt(dateFormat.format(c.getTime()));
+
+        chart = findViewById(R.id.chart);
 
         Description desc = chart.getDescription(); // initializing and setting a description
         desc.setText("");
@@ -100,10 +119,11 @@ public class Temperature extends AppCompatActivity {
         Legend legend = chart.getLegend(); // setting off a legend
         legend.setEnabled(false);
 
+        chart.setNoDataText("No data available for now, sorry about that :c ");
+        chart.setBorderWidth(100);
+
         XAxis xAxis = chart.getXAxis(); // setting size of chart
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setAxisMinimum(0f);
-        xAxis.setAxisMaximum(24);
         xAxis.setGranularity(1f);
         xAxis.setValueFormatter(new ValueFormatter() {
             /**
@@ -112,7 +132,7 @@ public class Temperature extends AppCompatActivity {
              */
             @Override
             public String getFormattedValue(float value) {
-                return MainActivity.getHours()[(int) value % MainActivity.getHours().length];
+                return MainActivity.getHours()[(int) (value + datetime) % MainActivity.getHours().length];
             }
         });
 
@@ -120,30 +140,79 @@ public class Temperature extends AppCompatActivity {
         leftAxis.setDrawGridLines(false);
         leftAxis.setAxisMinimum(0f);
         leftAxis.setAxisMaximum(50f);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setDrawLabels(false);
         chart.invalidate();
     }
 
+    public void getArrayListMinMax(ArrayList<Float> a) {
+        float minValue = a.get(0);
+        float maxValue = a.get(0);
+        for (float i : a) {
+            if (i < minValue) {
+                minValue = i;
+            }
+            if (i>maxValue){
+                maxValue = i;
+            }
+        }
+        minGraphValue = minValue-(float)0.5;
+        maxGraphValue = maxValue+(float)0.5;
+    }
+
+
     public void updateTempGraph() {
 
-        ArrayList test = getDayValueHistory();
+        ArrayList<Float> dayValues = getDayValueHistory();
+        getArrayListMinMax(dayValues);
 
-        LineChart chart = findViewById(R.id.chart); // plotting results
-        List<Entry> entries = new ArrayList<Entry>();
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMinimum(minGraphValue);
+        leftAxis.setAxisMaximum(maxGraphValue);
 
-        for (int i = 0; i < test.size(); i++) { // Filling list with x,y, where x is month and y is temp. value
-            entries.add(new Entry((float) i, (float) test.get(i)));
+        List<Entry> entries = new ArrayList<>();
+
+        for (int i = 0; i < 25; i++) { // Filling list with x,y, where x is month and y is temp. value;
+            entries.add(new Entry((float) i, dayValues.get(dayValues.size() / 25 * i)));
+            Log.d("", Float.toString(i));
         }
         LineDataSet dataSet = new LineDataSet(entries, "Temperature for the last year");
 
         dataSet.setColor(Color.rgb(0, 255, 0)); // adding line parameters
-        dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setDrawValues(false);
         dataSet.setLineWidth(2.5f);
-        dataSet.setCircleRadius(4f);
+        dataSet.setDrawCircles(false);
+        //dataSet.setCircleRadius(4f);
+
+        dataSet.setDrawFilled(true);
+        dataSet.setFillFormatter(new IFillFormatter() {
+            @Override
+            public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                return chart.getAxisLeft().getAxisMinimum();
+            }
+        });
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        //to enable the cubic density : if 1 then it will be sharp curve
+        dataSet.setCubicIntensity(0.2f);
+        dataSet.setFillColor(Color.BLACK);
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+        dataSet.setFillDrawable(drawable);
+
+        // set color of filled area
+        MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
+
+        // Set the marker to the chart
+        mv.setChartView(chart);
+        chart.setMarker(mv);
 
         LineData lineData = new LineData(dataSet);
 
         chart.setData(lineData);
-        chart.invalidate(); // refreshing chart
+        chart.animateY(2000, Easing.EaseInCubic);
+        chart.notifyDataSetChanged(); // refreshing chart
+        chart.invalidate();
     }
 }
 
